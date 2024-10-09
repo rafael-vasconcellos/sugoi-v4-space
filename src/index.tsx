463734b7c98d30b13863dcce3ef3e6a36c8df050
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 01:41:20 +0000

    chore: adjust tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 
-import './global.css'
+import './input.css'
 import App from './Home/App'
 
 const root = document.getElementById('root')
