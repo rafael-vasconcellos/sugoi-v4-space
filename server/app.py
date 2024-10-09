a3ed2ae294a5c424f3335c27d27bc58645a18601
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 05:22:59 +0000

    add route

--- server/app.py
+++ server/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return send_from_directory('dist', "index.html")
 
+@app.route('/<path:filename>')
+def download_file(filename):
+    return send_from_directory('.', filename)
+
 @app.route('/assets/<path:filename>')
 def download_file(filename):
     return send_from_directory('dist/assets', filename)
