9b014f02746e9e18723209dfff859840bd5e8ebe
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 01:45:38 +0000

    add proto interface

--- src/Home/App.tsx
+++ src/Home/App.tsx
@@ -5,7 +5,17 @@ export default function App() {
 
   return (
     <>
-      
+      <main class="w-screen flex bg-zinc-800">
+          <section class="w-1/2">
+            <div class="w-full px-2 flex justify-end">
+              <button>Translate</button>
+            </div>
+            {/* amber-500 */}
+            <textarea class="h-64 w-full" placeholder="Insert input text..." />
+          </section>
+
+          <textarea class="h-64 w-full" readonly />
+      </main>
     </>
   )
 }
