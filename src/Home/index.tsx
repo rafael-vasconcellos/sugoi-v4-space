ccdfe5f30b11830a2d2591e66286f4aa16da3ce9
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 02:52:37 +0000

    style: adjust the proto interface

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -4,19 +4,16 @@ import './style.css'
 export default function App() { 
 
   return (
-    <>
-      <main class="w-screen flex bg-zinc-800">
-          <section class="w-1/2">
-            <div class="w-full px-2 flex justify-end">
-              <button>Translate</button>
-            </div>
-            {/* amber-500 */}
-            <textarea class="h-64 w-full" placeholder="Insert input text..." />
-          </section>
-
-          <textarea class="h-64 w-full" readonly />
-      </main>
-    </>
+    <main class="w-screen my-24 mx-5">
+      <div class="w-1/2 p-3 flex justify-end">
+        <button>Translate</button>
+      </div>
+      <section class="flex">
+          {/* amber-500 */}
+          <textarea class="h-64 p-2 w-1/2 bg-zinc-700" placeholder="Insert input text..." />
+          <textarea class="h-64 p-7 w-1/2 text-2xl" placeholder='Translation' readonly />
+      </section>
+    </main>
   )
 }
 
