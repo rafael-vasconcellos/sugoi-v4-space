5f9624d69d982f54792b3e0f876e890972774c05
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 19:22:32 +0000

    feat: add route to the model

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, Response, request
 from waitress import serve
 from flask_cors import CORS, cross_origin
+from server.model import SugoiTranslator
 
 
 app = Flask(__name__)
@@ -15,8 +16,17 @@ def download_file(filename):
     return send_from_directory('.', filename)
 
 @app.route('/assets/<path:filename>')
-def download_file(filename):
+def download_assets(filename):
     return send_from_directory('dist/assets', filename)
+
+@app.route('api/translate', methods= ['POST'])
+@cross_origin()
+def translate_api(): 
+    text = request.args.get('text')
+    sugoiTranslator = SugoiTranslator()
+    if isinstance(text, str) and len(text) > 0: return json.dump(sugoiTranslator.translate(text))
+    return Response(status= 400)
+
 
 if __name__ == '__main__':
     serve(app)
