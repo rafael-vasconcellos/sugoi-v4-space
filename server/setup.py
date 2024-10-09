421f96d6547a2859bceab19ddd23e2cb5545ec7d
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-09 16:03:43 +0000

    adjust to hf model

--- server/setup.py
+++ server/setup.py
@@ -5,13 +5,13 @@ import ctranslate2
 
 def download_model():
     model_id = "JustFrederik/sugoi-v4-ja-en-ct2"
-    local_folder = snapshot_download(repo_id=model_id, local_dir="./fairseq_model")
+    local_folder = snapshot_download(repo_id=model_id, local_dir="./server/model")
     print(f"Files downloaded in: {local_folder}")
 
 def convert_model():    
     model_dir = "./fairseq_model"
     output_dir = "./server/ctranslate2_model"
-    if os.path.exists(output_dir) and "-overwrite" not in sys.argv: return
+    if (os.path.exists(output_dir) and "-overwrite" not in sys.argv) or not os.path.exists(model_dir): return
 
     print("Converting...")
     converter = ctranslate2.converters.FairseqConverter(model_dir)
@@ -22,4 +22,5 @@ def convert_model():
 
 if __name__ == "__name__":
     if "-download" in sys.argv: download_model()
-    convert_model()
+    if "-convert" in sys.argv: convert_model()
+
