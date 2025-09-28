af3af9ada957a29d0bc30452bca2c715a3d6327a
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2025-09-28 04:17:29 +0000

    chore: upgrade tailwindcss to version 4

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -33,15 +33,16 @@ export default function App() {
 
     return (
       <main class="w-screen my-24 mx-5">
-        <div class="w-1/2 py-4 px-2 flex justify-end">
-          <button onclick={() => {
+        <div class="w-1/2 py-4 px-2 flex justify-end gap-4">
+          <a class="button bg-zinc-500 hover:bg-zinc-500/60" href="/docs" target="_blank">Docs</a>
+          <button class="bg-primary hover:bg-primary-alt" onclick={() => {
               setText({ text: textarea?.value })
           }}>Translate</button>
         </div>
         <section class="flex">
             {/* amber-500 */}
             <textarea class="h-64 p-2 w-1/2 bg-zinc-700" placeholder="Insert input text..." ref={textarea} />
-            <textarea class="h-64 p-7 w-1/2 text-2xl" placeholder={translation.loading? "Translating..." : "Translation"} readonly value={translation()?.translatedText ?? ''} />
+            <textarea class="h-64 p-7 w-1/2 bg-inherit text-2xl" placeholder={translation.loading? "Translating..." : "Translation"} readonly value={translation()?.translatedText ?? ''} />
         </section>
       </main>
     )
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 
 export default defineConfig({
   plugins: [
     solid(),
+    tailwindcss(),
     viteStaticCopy({
         targets: [
           { src: 'public', dest: '.' } // copia tudo para dist/public/
